8e895104f9143fbfd7f21beb70a86171f004c808
milkslurpee <johnsuorsa@gmail.com>
2026-03-05 10:35:58 -0700

    done??? I have no idea why ci pipeline is working now I literally didn't change anything

--- generatePizzaData.sh
+++ generatePizzaData.sh
@@ -1,7 +1,7 @@
 # Check if host is provided as a command line argument
 if [ -z "$1" ]; then
   echo "Usage: $0 <host>"
-  echo "Example: $0 http://localhost:3000"
+  echo "Example: $0 https://pizza-service.mrpizzajohn.com"
   exit 1
 fi
 host=$1
