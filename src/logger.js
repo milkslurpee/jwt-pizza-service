1e2565e9efd9407c1e3bdc010f00206a9ce91edc
milkslurpee <johnsuorsa@gmail.com>
2026-03-23 18:28:11 -0600

    added mock to avoid testing errors

--- src/logger.js
+++ src/logger.js
@@ -1,11 +1,27 @@
-const { Logger } = require('pizza-logger');
+
 const config = require('./config');
 
-const logger = new Logger({
-  component: config.logging.source,
-  endpointUrl: config.logging.endpointUrl,
-  accountId: config.logging.accountId,
-  apiKey: config.logging.apiKey,
-});
+if (!config.logging || !config.logging.endpointUrl || !config.logging.apiKey) {
+  // Mock logger for tests (does nothing but prevents errors)
+  module.exports = {
+    httpLogger: (req, res, next) => next(),
+    dbLogger: () => {},
+    factoryLogger: () => {},
+    unhandledErrorLogger: () => {},
+    log: () => {},
+    error: () => {},
+  };
+} else {
 
-module.exports = logger;
+  const { Logger } = require('pizza-logger');
+
+  const logger = new Logger({
+    component: config.logging.source,
+    endpointUrl: config.logging.endpointUrl,
+    accountId: config.logging.accountId,
+    apiKey: config.logging.apiKey,
+  });
+
+  module.exports = logger;
+
+}
